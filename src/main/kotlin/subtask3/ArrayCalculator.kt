dc0788885a184b8f33f0704113ae9139e814c999
Weizhau <maks.malengo@gmail.com>
2020-05-06 22:52:01 +0300

    add ArrayProductCalculator

--- src/main/kotlin/subtask3/ArrayCalculator.kt
+++ src/main/kotlin/subtask3/ArrayCalculator.kt
@@ -5,18 +5,33 @@ class ArrayCalculator {
     // TODO: Complete the following function
     fun maxProductOf(numberOfItems: Int, itemsFromArray: Array<Any>): Int {
         var validNumbers = ArrayList<Int>();
-        var result: Int = 0
+        var result = 0
 
         itemsFromArray.filterIsInstanceTo(validNumbers, Int::class.javaObjectType)
 
         if (numberOfItems < validNumbers.size) {
-            if (validNumbers.size > 0) {
+            val outNumbers = ArrayList<Int>()
 
+            if (validNumbers.size > 0) {
+                for (i in 0..numberOfItems - 1) {
+                    if (Math.abs(validNumbers.max()!!) > Math.abs(validNumbers.min()!!)) {
+                        outNumbers.add(validNumbers.max()!!)
+                        validNumbers.remove(validNumbers.max()!!)
+                    } else {
+                        outNumbers.add(validNumbers.min()!!)
+                        validNumbers.remove(validNumbers.min()!!)
+                    }
+                }
+
+                while (countMinuses(outNumbers) % 2 != 0) {
+                    outNumbers.remove(outNumbers.min()!!)
+                    outNumbers.add(validNumbers.max()!!)
+                    validNumbers.remove(validNumbers.max()!!)
+                }
 
 
                 result = 1
-
-                for (num in validNumbers) {
+                for (num in outNumbers) {
                     result *= num
                 }
             }
@@ -32,5 +47,13 @@ class ArrayCalculator {
         }
 
         return result
+    }
+
+    fun countMinuses(arr: ArrayList<Int>): Int {
+        var count = 0
+        for (x in arr) {
+            if (x < 0) count++
+        }
+        return count
     }
 }
